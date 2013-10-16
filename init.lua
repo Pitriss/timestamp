61bafe788b7ea7760a48e327014f34729793ecdd
fairiestoy <johannesvk@web.de>
2013-10-17 00:20:58 +0200

    Push to v398864
    Included changes from pitriss. Thanks for pointing them out ;)

--- init.lua
+++ init.lua
@@ -2,25 +2,18 @@
 
 Seperated timestamp modification
 
-v: 333328
+v: 398864
+
+Latest changes:
+included suggestions from pitriss
 
 ]]
 
+local format_string = '%H:%M'
+
 function format_message( message )
-	local raw_time = os.date( '*t' )
-	local hours = nil
-	local minutes = nil
-	if raw_time.hour < 10 then
-		hours = '0'..raw_time.hour
-	else
-		hours = raw_time.hour
-	end
-	if raw_time.min < 10 then
-		minutes = '0'..raw_time.min
-	else
-		minutes = raw_time.min
-	end
-	return hours..':'..minutes..'] '..message
+	local raw_time = os.date( format_string )
+	return raw_time..'] '..message
 end
 
 minetest.register_on_chat_message( function( name, message )
@@ -32,3 +25,9 @@ minetest.register_on_chat_message( function( name, message )
 	end
 	return true
 end )
+
+local tmp_str_frmt = minetest.setting_get( 'time_format' )
+
+if tmp_str_frmt and not tonumber( tmp_str_frmt ) then
+	format_string = tmp_str_frmt
+end
